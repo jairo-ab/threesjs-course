8e61f10beb9fb3c5591dead6261477697fef37db
jairo-ab <jairo.abreu.3133@gmail.com>
2025-02-23 14:10:23 -0400

    [Physics] - Create ambient

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import gsap from 'gsap'
+import CANNON from 'cannon'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 
@@ -32,9 +33,70 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Textures
 const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.png',
+    '/textures/environmentMaps/0/nx.png',
+    '/textures/environmentMaps/0/py.png',
+    '/textures/environmentMaps/0/ny.png',
+    '/textures/environmentMaps/0/pz.png',
+    '/textures/environmentMaps/0/nz.png',
+])
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Sphere
+ */
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    new THREE.MeshStandardMaterial({
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5
+    })
+)
+
+sphere.castShadow = true
+sphere.position.y = 0.5
+scene.add(sphere)
+
+/**
+ * Floor (chão)
+ */
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+        color: '#777777',
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5
+    })
+)
+floor.receiveShadow = true
+floor.rotation.x = -Math.PI * 0.5
+scene.add(floor)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.1)
+scene.add(ambientLight)
+
+const directionLight = new THREE.DirectionalLight(0xffffff, 0.6)
+directionLight.castShadow = true
+directionLight.shadow.mapSize.set(1024, 1024)
+directionLight.shadow.camera.far = 15
+directionLight.shadow.camera.left = -7
+directionLight.shadow.camera.top = 7
+directionLight.shadow.camera.right = 7
+directionLight.shadow.camera.bottom = -7
+directionLight.position.set(5, 5, 5)
+scene.add(directionLight)
 
 // Sizes
 const sizes = {
@@ -44,7 +106,7 @@ const sizes = {
 
 window.addEventListener('resize', () => {
     // Atualização do tamanho da tela caso redimensionar
-    sizes.width =  window.innerWidth
+    sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
     // Atualização da câmera
@@ -59,7 +121,7 @@ window.addEventListener('resize', () => {
 // Camera
 const aspecRatio = sizes.width / sizes.height
 const camera = new THREE.PerspectiveCamera(75, aspecRatio, 0.1, 100)
-camera.position.z = 3
+camera.position.set(-3, 3, 3)
 scene.add(camera)
 
 // Controls
@@ -68,8 +130,6 @@ const controls = new OrbitControls(camera, canvas)
 // depois da atualização do próximo quadro
 controls.enableDamping = true
 
-controls.enabled = true
-
 //Render
 const renderer = new THREE.WebGLRenderer({
   canvas
@@ -77,6 +137,8 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
 // Clock
 const clock = new THREE.Clock()
