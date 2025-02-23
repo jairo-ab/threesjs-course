08783771de85f97faed1c9009a33d9e4c966e7c1
jairo-ab <jairo.abreu.3133@gmail.com>
2025-02-22 20:27:45 -0400

    [Galaxy] - Utilizado Points - BufferGeometry - E calculos para obter um resultado aceitavel

--- src/script.js
+++ src/script.js
@@ -36,6 +36,103 @@ const textureLoader = new THREE.TextureLoader()
 // Scene
 const scene = new THREE.Scene()
 
+/**
+ * Galaxy
+ */
+const parameters = {
+    count: 100000,
+    size: 0.01,
+    radius: 5,
+    branches: 3,
+    spin: 1,
+    randomness: 0.2,
+    randomnessPower: 3,
+    insideColor: '#ff6030',
+    outsideColor: '#1b3984'
+}
+
+let geometry = null
+let material = null
+let points = null
+
+const generateGalaxy = () => {
+    // Destroi as galaxias anteriores
+    if (points !== null) {
+        geometry.dispose()
+        material.dispose()
+        scene.remove(points)
+    }
+
+    geometry = new THREE.BufferGeometry()
+
+    const positions = new Float32Array(parameters.count * 3)
+    const colors = new Float32Array(parameters.count * 3)
+
+    const colorInside = new THREE.Color(parameters.insideColor)
+    const colorOutside = new THREE.Color(parameters.outsideColor)
+
+    for (let i = 0; i < parameters.count; i++) {
+        const i3 = i * 3
+
+        const radius = Math.random() * parameters.radius
+        const spinAngle = radius * parameters.spin
+        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
+
+        // Aleatoriedade
+        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1)
+        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1)
+        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1)
+
+        positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius + randomX
+        positions[i3 + 1] = 0 + randomY
+        positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ
+
+        // Cores
+        const mixedColor = colorInside.clone()
+        mixedColor.lerp(colorOutside, radius / parameters.radius)
+
+        colors[i3 + 0] = mixedColor.r
+        colors[i3 + 1] = mixedColor.g
+        colors[i3 + 2] = mixedColor.b
+    }
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+    /**
+     * Material
+     */
+    material = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        vertexColors: true
+    })
+
+    /**
+     * Points
+     */
+    points = new THREE.Points(geometry, material)
+
+    scene.add(points)
+}
+
+generateGalaxy()
+
+/**
+ * Gui
+ */
+gui.add(parameters, 'count').min(100).max(100000).step(100).onFinishChange(() => generateGalaxy())
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(() => generateGalaxy())
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(() => generateGalaxy())
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(() => generateGalaxy())
+gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(() => generateGalaxy())
+gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(() => generateGalaxy())
+gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(() => generateGalaxy())
+gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy())
+gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy())
+
 // Sizes
 const sizes = {
   width: window.innerWidth,
@@ -59,7 +156,9 @@ window.addEventListener('resize', () => {
 // Camera
 const aspecRatio = sizes.width / sizes.height
 const camera = new THREE.PerspectiveCamera(75, aspecRatio, 0.1, 100)
-camera.position.z = 3
+camera.position.x = 6
+camera.position.y = 6
+camera.position.z = 6
 scene.add(camera)
 
 // Controls
@@ -84,6 +183,8 @@ const clock = new THREE.Clock()
 //Animations
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+
+    points.rotation.y = elapsedTime * 0.2
 
     // Atualização do controle
     controls.update()
