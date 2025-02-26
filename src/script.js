f4b850f511443b01b275ae4f1b6d072453d9a6de
jairo-ab <jairo.abreu.3133@gmail.com>
2025-02-25 22:44:04 -0400

    [Models] - Adicionado modelos gltf - bin - draco, etc - Utilizando animações do modelo raposa (Fox) - Exemplos de uso para adicionar na scene

--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@ import './style.css'
 import * as THREE from 'three'
 import gsap from 'gsap'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
 import GUI from 'lil-gui'
 
 // Debug
@@ -36,15 +38,89 @@ const textureLoader = new THREE.TextureLoader()
 // Scene
 const scene = new THREE.Scene()
 
-// Plane
-const plane_floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshBasicMaterial({ color: 0xffffff })
+/**
+ * GLTFLoader Model
+ */
+
+const models = {
+    duck_gltf: '/models/Duck/glTF/Duck.gltf',
+    duck_binary: '/models/Duck/glTF-Binary/Duck.glb',
+    duck_embedded: '/models/Duck/glTF-Embedded/Duck.gltf',
+    duck_gltf_draco: '/models/Duck/glTF-Draco/Duck.gltf',
+
+    // FlightHelmet
+    flightHelmeted_gltf: '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+
+    // Fox
+    fox_gltf: '/models/Fox/glTF/Fox.gltf'
+}
+
+const gltfLoader = new GLTFLoader()
+const dracoLoader = new DRACOLoader()
+
+dracoLoader.setDecoderPath('/draco/')
+gltfLoader.setDRACOLoader(dracoLoader)
+
+let mixer = null
+
+gltfLoader.load(
+    models.fox_gltf,
+    function(gltf) {
+        console.log('Load file: ', gltf)
+
+        // const children = [...gltf.scene.children]
+        // for (const child of children) {
+        //     scene.add(child)
+        // }
+
+        mixer = new THREE.AnimationMixer(gltf.scene)
+        const action = mixer.clipAction(gltf.animations[2])
+        action.play()
+
+        gltf.scene.scale.set(0.025, 0.025, 0.025)
+        scene.add(gltf.scene)
+    },
+    function() {
+        console.log('Progress loading')
+    },
+    function(error) {
+        console.log('Error: ', error)
+    }
 )
 
+/**
+ * Plane Floor
+ */
+const plane_floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+        color: '#444444',
+        metalness: 0.3,
+        roughness: 0.4
+    })
+)
+
+plane_floor.receiveShadow = true
 plane_floor.rotation.x = -Math.PI * 0.5
 
 scene.add(plane_floor)
+
+/**
+ * Light
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
 
 // Sizes
 const sizes = {
@@ -69,7 +145,7 @@ window.addEventListener('resize', () => {
 // Camera
 const aspecRatio = sizes.width / sizes.height
 const camera = new THREE.PerspectiveCamera(75, aspecRatio, 0.1, 100)
-camera.position.set(2, 3, 3)
+camera.position.set(2, 2, 2)
 scene.add(camera)
 
 // Controls
@@ -90,10 +166,16 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 // Clock
 const clock = new THREE.Clock()
+let oldTime = 0
 
 //Animations
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - oldTime
+    oldTime = elapsedTime
+
+    // Animation
+    mixer?.update(deltaTime)
 
     // Atualização do controle
     controls.update()
